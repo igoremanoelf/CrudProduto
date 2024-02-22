ddaa150d9698c0f8c0bb8a053ce2fd7d34cea2c6
Igor Emanoel <147579291+igoremanoelf@users.noreply.github.com>
2024-02-22 14:09:44 -0300

    Add files via upload

---
+++ CrudProduto/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿let table1 = new DataTable('#tabelaProdutos', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
+
+let table2 = new DataTable('#tabelaCategorias', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
+
+let table3 = new DataTable('#tabelaFornecedores', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
